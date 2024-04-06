b8ce0c638a1b02e511fdf93d361942b9500209d8
Etienne Delay <etienne.delay@cirad.fr>
2024-04-06 17:44:52 +0000

    quik and durty plotly script for pse results

---
+++ scriptR/m0_PSE_plotly.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+df <- read.csv("../results_pse/population1720_percentile5.csv", header = T)
+
+names(df)
+
+ggplot(df, aes(x = objective.om_capitalTotal, y = objective.om_sumBiomass, colour = nbBoats))+
+  geom_point()+
+  theme_bw()
+
+
+ggplot(df, aes(x = objective.om_capitalTotal, y = objective.om_sumBiomass, colour = ReserveIntegrale))+
+  geom_point()+
+  theme_bw()
+
+
+library(plotly)
+plot_ly(x = df$objective.om_capitalTotal, y = df$objective.om_sumBiomass, z=df$nbBoats, 
+        type="scatter3d", 
+        mode="markers", 
+        color=df$ReserveIntegrale,
+        size = 5)%>%
+layout( scene = list(xaxis = list(title = 'Capital'),
+                       yaxis = list(title = 'Biomass'),
+                       zaxis = list(title = 'nb boat')),
+        legend = list(title = list(text = 'Espèce')))
+
+
