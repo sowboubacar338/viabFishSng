82935f6aa75f121810bcdb2989951339b45d2129
Etienne Delay <etienne.delay@cirad.fr>
2024-04-06 17:50:54 +0000

    filtre on sample

--- scriptR/m0_PSE_plotly.R
+++ scriptR/m0_PSE_plotly.R
@@ -7,15 +7,15 @@ df <- read.csv("../results_pse/population1720_percentile5.csv", header = T)
 
 names(df)
 
-ggplot(df, aes(x = objective.om_capitalTotal, y = objective.om_sumBiomass, colour = nbBoats))+
+ggplot(df, aes(x = objective.om_capitalTotal, y = objective.om_sumBiomass, colour = nbBoats, size = evolution.samples))+
   geom_point()+
   theme_bw()
 
 
-ggplot(df, aes(x = objective.om_capitalTotal, y = objective.om_sumBiomass, colour = ReserveIntegrale))+
+ggplot(df, aes(x = objective.om_capitalTotal, y = objective.om_sumBiomass, colour = ReserveIntegrale, size = evolution.samples))+
   geom_point()+
   theme_bw()
-
+df <- df[df$evolution.samples>1,]
 
 library(plotly)
 plot_ly(x = df$objective.om_capitalTotal, y = df$objective.om_sumBiomass, z=df$nbBoats, 
